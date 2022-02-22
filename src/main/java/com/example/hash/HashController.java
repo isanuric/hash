a2f93d6b21df57d636b27051ca0a94c2d38b088d
Ehsan Salmani <isanuric@protonmail.com>
2022-02-22 19:58:44 +0100

    add http client

--- src/main/java/com/example/hash/HashController.java
+++ src/main/java/com/example/hash/HashController.java
@@ -1,6 +1,5 @@
 package com.example.hash;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
